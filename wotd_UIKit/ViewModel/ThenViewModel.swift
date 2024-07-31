e7a62fba44c38b2756f3a4b5db821c791a188e84
emilyj4482 <emilyj4482@gmail.com>
2024-08-01 04:23:14 +0900

    now view model에서 얻은 오늘 날씨 정보를 combine을 이용하여 then view model로 전송하기 setting

--- wotd_UIKit/ViewModel/ThenViewModel.swift
+++ wotd_UIKit/ViewModel/ThenViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 protocol ThenViewDelegate {
     func pushComparisionViewController(with weather: ThenWeather)
@@ -14,14 +15,39 @@ protocol ThenViewDelegate {
 final class ThenViewModel: ObservableObject {
     static let shared = ThenViewModel()
     
+    private var subscriptions = Set<AnyCancellable>()
+    
     let isEmpty: Bool = false
     
     @Published var weathers: [ThenWeather] = []
     
-    @Published var nowWeather: ThenWeather
+    /*
+    @Published var nowWeather: ThenWeather {
+        didSet {
+            print(nowWeather)
+        }
+    }
+    */
     
-    init(nowWeather: ThenWeather = ThenWeather(date: Date(), city: "Suwon-si", min: 10, max: 23, morning: 10, afternoon: 23, evening: 17, night: 11)) {
-        self.nowWeather = nowWeather
+    init() {
         weathers.append(ThenWeather(date: Date() - 2592000, city: "London", min: 7, max: 16, morning: 9, afternoon: 16, evening: 11, night: 7))
+    }
+    
+    func bind() {
+        let vm = NowViewModel.shared
+        
+        vm.location
+            .combineLatest(vm.weatherInfo)
+            .receive(on: DispatchQueue.main)
+            .map { (location, weatherInfo) in
+                return (location, weatherInfo.temperature)
+            }
+            .map { location, temp in
+                return ThenWeather(date: Date(), city: location, min: temp.max.toInt, max: temp.max.toInt, morning: temp.morning.toInt, afternoon: temp.afternoon.toInt, evening: temp.evening.toInt, night: temp.night.toInt)
+            }
+            .sink { thenWeather in
+                // 이걸 어케 하면 nowWeather에 주입할 수 있을까
+            }
+            .store(in: &subscriptions)
     }
 }
