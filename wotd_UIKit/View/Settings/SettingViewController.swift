4e5a0e4cdc5cc9de4cc7f74ddaa6902b7c6184a6
emilyj4482 <emilyj4482@gmail.com>
2024-08-03 23:02:07 +0900

    setting view 초기 구성

--- wotd_UIKit/View/Settings/SettingViewController.swift
+++ wotd_UIKit/View/Settings/SettingViewController.swift
@@ -6,11 +6,57 @@
 //
 
 import UIKit
+import SnapKit
 
 class SettingViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .accent
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubviews()
+        layout()
     }
     
+    private func addSubviews() {
+        view.addSubview(tableView)
+    }
+    
+    private func layout() {
+        view.backgroundColor = .descent
+        
+        tableView.snp.makeConstraints {
+            $0.top.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+        }
+    }
+}
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
+}
+
+#Preview {
+    SettingViewController()
 }
